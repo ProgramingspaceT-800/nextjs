7fafc906a1b31d0501493a8a2873c9bf902493ba
ProgramingspaceT-800 15 <andreiaaraujodemelo@gmail.com>
2023-10-18 23:42:19 +0000

    commitchanges

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,178 @@
-import Image from 'next/image'
+"use client"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, CardContent } from './stylespage';
+import logoPlataforma from '../assets/logo plataforma.png';
 
-export default function Home() {
+const Home: React.FC = () => {
+  type BasesType = {
+    [key: string]: string;
+  };
+
+  type PostType = {
+    id: number;
+    name: string;
+    created_at: string;
+    completed_percentage: string;
+    weight: number; // Adicione o campo 'weight'
+    // Adicione outros campos conforme necessário
+  };
+
+  const [bases] = useState<BasesType>({
+     'G - FGTS': '29870',
+    'C e M - INSS PORTABILIDADE ATUALIZADA FILE': '78025',
+    'M - UNIFICADA GRUPO SP - GOV/PME/SPPREV': '80797',
+    'G - INSS PORTABILIDADE': '69098',
+    'C - BASE FILE PORT INBURSA/CREFISA': '83106',
+    'I - TJSP': '62048',
+    'C e M - INBURSA PORTANDO D+ BANCOS PARA: ACIMA DE 500': '83752',
+    'G - PMESP AUMENTO 20%': '70014',
+    'M - GOV MT GERAL': '70967',
+    'C - INSS PORTABILIDADE INBURSA': '80802',
+    'G - PMSP - PREMIUM': '80801',
+    'M - SANTOS': '82286',
+    'I - BANCO MASTER SAQUE COMPL SEFAZ': '54283',
+    'G - PMSP AUMENTO 5%': '70015',
+    'G - OPORTUNIDADE INSS BANCO MASTER': '30730',
+    'C - INSS PORTABILIDADE CREFISA': '80803',
+    'S - UNIFICADA GRUPO SP - GOV/PME/SPPREV': '80798',
+    'I - PREF CURITIBA': '80595',
+    'M - GOV PARANA SAQUE COMPLEMENTAR MASTER': '79162',
+    'G - MARGEM REAL CARTÃO BENEFÍCIO MCC GOVSP': '70034',
+    'I - PM BETIM': '60139',
+    'G - GOV MARANHAO CARTAO BENEFICIO': '59136',
+    'G - GOV MG - GERAL': '55691',
+    'G - GOV PE': '51370',
+    'G - MT GROSSO DO SUL GERAL': '50943',
+    'G - SAQUE COMPLEMENTAR DAYCOVAL ACIMA 1000': '50008',
+    'G - REFIN SANTANDER | REDUÇÃO TAXA | PUBLICOS': '45169',
+    'G - SAQUE COMPLEMENTAR BANCO MASTER ATUALIZADO': '41975',
+    'G - COMLURB SANT': '41000',
+    'G - REFIN SANTANDER UNIFICADA': '38247',
+    'G - GOV SANTA CATARINA': '37114',
+    'G - GOV RJ CORRENTISTAS': '35994',
+    'G - OSASCO': '33270',
+    'G - 50 + VENDIDOS MARGEM ONLINE': '33263',
+  });
+
+   const [loading, setLoading] = useState(true);
+  const [baseData, setBaseData] = useState<{ [key: string]: PostType[] }>({});
+  const [expandedBase, setExpandedBase] = useState<string | null>(null);
+  const [campanhasComAviso, setCampanhasComAviso] = useState<string[]>([]);
+  const [lastUpdate, setLastUpdate] = useState(new Date());
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const newData: { [key: string]: PostType[] } = {};
+
+        for (const baseName in bases) {
+          const id = bases[baseName];
+          const response = await axios.get(`https://3c.fluxoti.com/api/v1/campaigns/${id}/lists?api_token=d0NLCpTnvtsY1gQu7S38RyF47fOjnHknynBjGzWxCwpXOJqXaNwWDrGqFomq`);
+          const filteredData = response.data.data.filter((post: PostType) => post.weight === 1);
+          newData[id] = filteredData;
+
+          const temPorcentagemMaiorQue90 = filteredData.some((post: PostType) => parseFloat(post.completed_percentage) > 90);
+          if (temPorcentagemMaiorQue90) {
+            setCampanhasComAviso((prevCampanhas) => [...prevCampanhas, id]);
+          }
+        }
+
+        setCampanhasComAviso(campanhasComAviso);
+        setBaseData(newData);
+        setLoading(false);
+        setLastUpdate(new Date());
+      } catch (error) {
+        console.error('Erro ao buscar os dados:', error);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+    const interval = setInterval(fetchData, 1 * 30 * 1000); // Recarrega a cada 30 segundos
+
+    return () => clearInterval(interval); // Limpa o temporizador quando o componente é desmontado
+  }, [bases]);
+
+  function renderPorcentagem(porcentagem: string) {
+    const porcentagemNum = parseFloat(porcentagem);
+
+    if (porcentagemNum === 100) {
+      const porcentagemFormatada = porcentagemNum.toFixed(0);
+      return <span className="porcentagem-azul">{porcentagemFormatada}%</span>;
+    } else if (porcentagemNum >= 90 && porcentagemNum <= 99.99) {
+      const porcentagemFormatada = porcentagemNum.toFixed(0);
+      return <span className="porcentagem-vermelha">{porcentagemFormatada}%</span>;
+    } else {
+      const porcentagemFormatada = porcentagemNum.toFixed(0);
+      return <span className="porcentagem-verde">{porcentagemFormatada}%</span>;
+    }
+  }
+
+  function toggleDetails(id: string) {
+    if (expandedBase === id) {
+      setExpandedBase(null);
+    } else {
+      setExpandedBase(id);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <CardContent>
+      <div className="principalContent">
+        <div className="logo">
         </div>
+        <nav>
+          <ul className="nav-links">
+            <li><a href="/">Bases</a></li>
+            <li className="center"><a href="#">Graficos</a></li>
+            <li className="upward"><a href="#">Adicionar</a></li>
+            <li className="forward"><a href="#">Update</a></li>
+          </ul>
+        </nav>
+        {loading ? (
+          <p>Carregando...</p>
+        ) : (
+          <div className="Cardsalign">
+            {Object.keys(bases).map((baseName) => (
+              <div
+                key={baseName}
+                className={`campaign-card${expandedBase === bases[baseName] ? ' clicked' : ''}`}
+                onClick={() => toggleDetails(bases[baseName])}
+              >
+<div className={`base ${baseData[bases[baseName]] && baseData[bases[baseName]].length > 0 && parseFloat(baseData[bases[baseName]][0].completed_percentage) >= 90 ? 'com-aviso' : ''}`}>{baseName}</div>
+                {expandedBase === bases[baseName] && (
+                  <div className="campaign-percentages">
+                    {baseData[bases[baseName]]?.map((post) => (
+                      <Container key={post.id}>
+                        <div
+                          className={`percentage-item ${parseFloat(post.completed_percentage) >= 90
+                            ? 'com-aviso'
+                            : ''
+                          }`}
+                        >
+                          <h3>{post.name}</h3>
+                          <p>{post.created_at}</p>
+                          <h3 className={`porcentagem-${post.completed_percentage === '100.00' ? 'verde' : 'vermelha'}`}>
+                            {renderPorcentagem(post.completed_percentage)}
+                          </h3>
+                          <p>Última atualização: {lastUpdate.toLocaleTimeString()}</p>
+                        </div>
+                      </Container>
+
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
       </div>
+    </CardContent>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default Home;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
---
+++ app/stylespage.ts
@@ -0,0 +1,206 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+
+export const CardContent = styled.div`
+.principalContent {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.Cardsalign {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.campaign-card {
+  border: 1px solid #ccc;
+  margin: 10px;
+  padding: 20px;
+  width: 40%; /* 48% para deixar um espaço mínimo entre as duas divs */
+  margin-bottom: 20px;
+  gap: 20px;
+  border-radius: 5px;
+  transition: transform 0.2s ease-in-out;
+  box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5);
+    background: rgba(255, 255, 255, 0.1); 
+    justify-content: center;
+    align-items: center;
+    border-top: 1px solid rgba(255, 255, 255, 0.5);
+    border-left: 1px solid rgba(255, 255, 255, 0.5);
+}
+
+.base {
+  font-size: 20px;
+	color: white;
+  font-weight: bold;
+}
+
+h3 { 
+  color: white;
+}
+
+p {
+  color: white;
+}
+
+.campaign-card:hover {
+  transform: scale(1.05);
+  cursor: pointer;
+  box-shadow: 0 0 8px #fd0018, 0 0 35px #fd0018, 0 0 60px #fd0018;
+}
+
+.campaign-card.clicked {
+  background: rgba(255, 255, 255, 0.1); 
+    justify-content: center;
+    align-items: center;
+    border-top: 1px solid rgba(255, 255, 255, 0.5);
+    border-left: 1px solid rgba(255, 255, 255, 0.5);
+}
+
+/* Estilo dos detalhes de campanha expandidos */
+.campaign-percentages {
+  display: none;
+}
+
+.campaign-card.clicked .campaign-percentages {
+  display: block;
+  margin-top: 10px;
+}
+
+.percentage-item {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 10px;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden; /* Esconde o conteúdo que transborda */
+  text-overflow: ellipsis; /* Mostra "..." para indicar que o texto foi cortado */
+  white-space: nowrap; 
+}
+
+.title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center; 
+}
+
+h1 {
+  margin-bottom: 3%;
+}
+
+.percentage-item h3 {
+  font-size: 16px;
+  margin: 5px 0;
+}
+
+.com-aviso {
+  border: 2px solid red;
+  box-shadow: 0 0 1px #fd0018, 0 0 15px #fd0018, 0 0 30px #fd0018; /* Efeito de sombra vermelha */}
+}
+
+/* Estilos para porcentagens */
+.porcentagem-vermelha {
+  color: red;
+  font-weight: bold;
+}
+
+.porcentagem-verde {
+  color: green;
+  font-weight: bold;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+    width: 100%;
+}
+
+.nav-links{
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  align-items: center;
+  background: #fff;
+  padding: 20px 15px;
+  border-radius: 12px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.2);
+  margin-top: 3%;
+  margin-bottom: 3%;
+}
+
+.nav-links:hover {
+  box-shadow: 0 0 8px #fd0018, 0 0 35px #fd0018, 0 0 60px #fd0018;
+}
+
+.nav-links li{
+  list-style: none;
+  margin: 0 12px;
+}
+.nav-links li a{
+  position: relative;
+  color: black;
+  font-size: 25px;
+  font-family:'open_sansregular';  
+  font-weight: bold;
+  padding: 6px 0;
+  text-decoration: none;
+}
+.nav-links li a:before{
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3px;
+  width: 0%;
+  background: red;
+  border-radius: 12px;
+  transition: all 0.4s ease;
+}
+.nav-links li a:hover:before{
+  width: 100%;
+}
+.nav-links li.center a:before{
+  left: 50%;
+  transform: translateX(-50%);
+}
+.nav-links li.upward a:before{
+  width: 100%;
+  bottom: -5px;
+  opacity: 0;
+}
+.nav-links li.upward a:hover:before{
+  bottom: 0px;
+  opacity: 1;
+}
+.nav-links li.forward a:before{
+  width: 100%;
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 0.4s ease;
+}
+.nav-links li.forward a:hover:before{
+  transform: scaleX(1);
+  transform-origin: left;
+}
+
+.logo {
+  display: flex;
+  max-height: 150px;
+  width: 100%;
+  height: auto;
+  justify-content: center;
+  align-items: center;
+}
+
+
+`
